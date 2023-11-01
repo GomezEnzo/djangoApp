a451a86af24946d4204cd13bfc7ee6bd8856727d
GomezEnzo <83846047+GomezEnzo@users.noreply.github.com>
2023-11-01 17:44:23 -0300

    Update settings.py

--- TestApp/settings.py
+++ TestApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nod)y7%t)+c_0#bk*zt@y5gcd9b7q8iz#vd!5dq0n@rffwcpn^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
